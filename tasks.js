3a85907d10fc8d2d99f6abca524e76a4c609f8cc
Fonri <121763532+Fonri@users.noreply.github.com>
2023-05-02 20:47:59 +0400

    Search added

--- tasks.js
+++ tasks.js
@@ -1,22 +1,26 @@
-function handleTaskClick(e){
+function handleTaskClick(e) {
   const currentTask = e.currentTarget;
 
   currentTask.classList.toggle("completed");
-  const currentTaskId = currentTask.getAttribute('data-id'); 
+  const currentTaskId = currentTask.getAttribute("data-id");
 
   const tasks = getTasksFromLocalStorage();
 
-  const updatedTasks = tasks.map(task => task.id === currentTaskId ? {
-    ...task,
-    isCompleted: currentTask.classList.contains("completed"),
-  } : task);
+  const updatedTasks = tasks.map((task) =>
+    task.id === currentTaskId
+      ? {
+          ...task,
+          isCompleted: currentTask.classList.contains("completed"),
+        }
+      : task
+  );
 
   setTasksToLocalStorage(updatedTasks);
 }
 
 async function addTask() {
   const input = document.getElementById("taskInput");
-  if(!input.value.length) return;
+  if (!input.value.length) return;
 
   const tasksList = document.getElementById("tasksList");
   const newTaskId = Math.random().toString();
@@ -29,18 +33,20 @@ async function addTask() {
 
   const tasks = getTasksFromLocalStorage();
 
-  setTasksToLocalStorage([{
-    id: newTaskId,
-    isCompleted: false,
-    textContent: input.value,
-  }, ...tasks]);
+  setTasksToLocalStorage([
+    {
+      id: newTaskId,
+      isCompleted: false,
+      textContent: input.value,
+    },
+    ...tasks,
+  ]);
 
   await showLoading();
 
   tasksList.prepend(newTask);
   input.value = "";
 }
-
 
 async function toggleList() {
   const tasksList = document.getElementById("tasksList");
@@ -50,10 +56,18 @@ async function toggleList() {
     await showLoading();
     const tasks = getTasksFromLocalStorage();
 
-    tasksList.innerHTML = '';
-    tasks.forEach(task => {
-      const tasksListElement = createNewTask({...task, onClick: handleTaskClick})
-      tasksList.appendChild(tasksListElement);
+    const searchInput = document.getElementById("searchInput");
+    const searchTerm = searchInput.value.toLowerCase();
+
+    tasksList.innerHTML = "";
+    tasks.forEach((task) => {
+      if (task.textContent.toLowerCase().includes(searchTerm)) {
+        const tasksListElement = createNewTask({
+          ...task,
+          onClick: handleTaskClick,
+        });
+        tasksList.appendChild(tasksListElement);
+      }
     });
 
     tasksList.style.display = "block";
@@ -64,8 +78,36 @@ async function toggleList() {
   }
 }
 
-const addButton = document.getElementById('addButton');
-const toggleButton = document.getElementById('toggleButton');
+const addButton = document.getElementById("addButton");
+const toggleButton = document.getElementById("toggleButton");
 
-addButton.addEventListener('click', addTask);
-toggleButton.addEventListener('click', toggleList);
+addButton.addEventListener("click", addTask);
+toggleButton.addEventListener("click", toggleList);
+
+async function handleSearchClick() {
+  const searchInput = document.getElementById("searchInput");
+  const searchText = searchInput.value.toLowerCase();
+
+  const tasks = getTasksFromLocalStorage();
+
+  const filteredTasks = tasks.filter((task) =>
+    task.textContent.toLowerCase().includes(searchText)
+  );
+
+  const tasksList = document.getElementById("tasksList");
+
+  tasksList.innerHTML = "";
+
+  filteredTasks.forEach((task) => {
+    const tasksListElement = createNewTask({
+      ...task,
+      onClick: handleTaskClick,
+    });
+    tasksList.appendChild(tasksListElement);
+  });
+}
+
+const searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click", handleSearchClick);
+
